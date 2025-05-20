a118155333316f577386e813453e57c457d87c15
agnes.varnlund@hotmail.com <agnes.varnlund@hotmail.com>
2025-05-20 14:49:32 +0200

    Examinationsuppgift

--- src/script.js
+++ src/script.js
@@ -11,14 +11,15 @@ let income = []
 let expenses = []
 
 function addTransaction(type) {
-  const text = desc.value
-  const money = amount.value
+  
+const text = desc.value
+const money = amount.value
 
-  if (text === "" || money === "") {
-    return
-  }
+if (text === "" || money === "") {
+return
+}
 
-  const transaction = {
+const transaction = {
     description: text,
     amount: Number(money),
     type: type,
@@ -39,22 +40,23 @@ function addTransaction(type) {
 }
 
 function showIncome() {
-  incomeList.innerHTML = ""
-  income.forEach((item) => {
-    const li = document.createElement("li")
-    li.textContent = item.description + ": +" + item.amount + " kr"
-    incomeList.appendChild(li)
-  })
-}
+
+incomeList.innerHTML = ""
+income.forEach((item) => {
+      const li = document.createElement("li")
+      li.textContent = `${item.description} + ${item.amount} kr`
+      incomeList.appendChild(li)
+    })
+  }
 
 function showExpenses() {
-  expenseList.innerHTML = ""
-  expenses.forEach((item) => {
-    const li = document.createElement("li")
-    li.textContent = item.description + ": -" + item.amount + " kr"
-    expenseList.appendChild(li)
-  })
-}
+    expenseList.innerHTML = ""
+    expenses.forEach((item) => {
+      const li = document.createElement("li")
+      li.textContent = `${item.description} - ${item.amount} kr`
+      expenseList.appendChild(li)
+    })
+  }
 
 function updateBalance() {
   let totalIncome = 0
